039694eeee8ceb720cd410ce104ab80553de933b
BarBlaBla <67729419+BarBlaBla@users.noreply.github.com>
2020-07-09 14:43:08 +0700

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,24 @@
+#!binbash
+
+# Exit early on errors
+set -eu
+
+# Python buffers stdout. Without this, you won't see what you print in the Activity Logs
+export PYTHONUNBUFFERED=true
+
+# Install Python 3 virtual env
+VIRTUALENV=.datavenv
+
+if [ ! -d $VIRTUALENV ]; then
+  python3 -m venv $VIRTUALENV
+fi
+
+if [ ! -f $VIRTUALENVbinpip ]; then
+  curl --silent --show-error --retry 5 httpsbootstrap.pypa.ioget-pip.py  $VIRTUALENVbinpython
+fi
+
+# Install the requirements
+$VIRTUALENVbinpip install -r requirements.txt
+
+# Run a glorious Python 3 server
+$VIRTUALENVbinpython3 app.py
